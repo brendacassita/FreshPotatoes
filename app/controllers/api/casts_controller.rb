f09a65c77978a41f14bd0708a82f269e2e9142a8
Jessica Nguyen <jessica.stevens2115@gmail.com>
2022-04-11 15:07:11 -0600

    basic crud for casts controller and reviews controller

--- app/controllers/api/casts_controller.rb
+++ app/controllers/api/casts_controller.rb
@@ -1,3 +1,40 @@
 class Api::CastsController < ApplicationController
+    before_action :set_cast, only: [:update, :show, :destroy]
+
+    def index
+        render json: Cast.all
+    end
+
+    def create
+        cast = Cast.new(cast_params)
+        if(cast.save)
+            render json: cast
+        else
+            render json: {error:cast.error.full_messages}, status: 422
+        end
+    end
+
+    def update
+        if(@cast.update(cast_params))
+            render json: @cast
+        else
+            render json: {error:@cast.errors.full_messages}, status: 422
+        end
+    end
+    
+    def destroy
+        render json: @cast.destroy
+    end
+
+
+    private
+
+    def set_cast
+        @cast = Cast.find(params[:id])
+    end
+
+    def cast_params
+        params.require(:cast).permit(:headshot, :name)
+    end
 
 end
--- app/controllers/api/reviews_controller.rb
+++ app/controllers/api/reviews_controller.rb
@@ -1,2 +1,40 @@
 class Api::ReviewsController < ApplicationController
+    before_action :set_review, only: [:update, :show, :destroy]
+
+    def index
+        render json: Review.all
+    end
+
+    def create
+        review = Review.new(review_params)
+        if(review.save)
+            render json: review
+        else
+            render json: {error:review.errors.full_messages},   status: 422
+        end
+    end
+
+    def update
+        if(@review.update(reivew_params))
+            render json: @review
+        else
+            render json: {error:@review.errors.full_messages}, status: 422
+        end
+    end
+
+    def destroy
+        render json: @review.destroy
+    end
+
+
+    private
+
+    def set_review
+        @review = Review.find(params[:id])
+    end
+
+    def review_params
+        params.require(:review).permit(:rating, :watched)
+    end
+
 end
