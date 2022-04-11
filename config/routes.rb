24a3c17ea2aca618c723b3b959c31d8637b5f386
Brenda <brendacassita@gmail.com>
2022-04-11 15:46:42 -0600

    added to routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,12 @@ Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'api/auth'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
    namespace :api do 
+    resources :movies
+    resources :reviews
+    resources :casts
+    resources :roles
+
+
      put 'users/update_image', to: "users#update_image"
    end
 end
