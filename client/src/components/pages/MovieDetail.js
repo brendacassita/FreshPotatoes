58977ea4541a729e01a1d33694570b16ce422f22
Brenda <brendacassita@gmail.com>
2022-04-12 19:18:00 -0600

    init

---
+++ client/src/components/pages/MovieDetail.js
@@ -0,0 +1,16 @@
+const MovieDetail = () =>{
+  
+  
+  
+  
+  
+  
+  return(
+    <div>
+      <h1>Movie Detail</h1>
+      {/* <img src={movie.poster} width={200} /> */}
+    </div>
+  )
+}
+
+export default MovieDetail
