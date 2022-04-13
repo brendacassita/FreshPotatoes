3cf216d33e22c08576a4fd509ba936d649d0ea29
Sharayah Hefner <rocknrayah@gmail.com>
2022-04-13 14:43:54 -0600

    searchResults loaded needs more functionality

--- client/src/components/pages/SearchResults.js
+++ client/src/components/pages/SearchResults.js
@@ -6,9 +6,9 @@ import Fuse from 'fuse.js'
 
 
 const SearchResults = () => {
-  const [allMovies,setAllMovies] = useState(null)
+  const [allMovies,setAllMovies] = useState([])
   const [loading, setLoading] = useState(true)
-  const [filterMovies,setFilteredMovie]  = useState([])
+  const [filteredMovies,setFilteredMovies]  = useState([])
   const [searchTerm,setSearchTerm]  = useState('')
   
   useEffect(() => {
@@ -20,39 +20,57 @@ const SearchResults = () => {
     includeScore: true,
     keys:['name', 'genre']
   }
-  const fuse = new Fuse(options)
+  const fuse = new Fuse(allMovies, options)
  
   
   const getMoviesFromApi = async () => {
     try {
       let res = await axios.get('/api/movies')
       setAllMovies(res.data)
+      setFilteredMovies(res.data)
     } catch(err) {
       alert('error getting movies')
     }
   }
   
+  
+  
   const handleSearchTermChange = (e)=>{
      setSearchTerm(e.target.value)
-     const result = fuse.search(e.target.value)
-    setFilteredMovie(result)
-    // normalize render whatever you need
+    const result = fuse.search(e.target.value)
+   
+    // Mapping through the results and displaying only y.item
+    let MovieData = result.map(findMovie => findMovie.item)
+   
+    setFilteredMovies(MovieData)
     
   }
+  
+  
 
   return (
-    <div>
-      
+    <div className='App'>
       <h1> Search Result Page</h1>
-      <div className='ui icon input'>
-        <p>Search:</p>
-        <input onChange={handleSearchTermChange} type='text' placeholder='search movies'></input>
+      <p>Search:</p>
+      <input onChange={handleSearchTermChange} value={searchTerm} type='text' placeholder='search movies'></input>
+        <p>{JSON.stringify(filteredMovies)}</p>
+      
+      <hr></hr>
+      {allMovies.map((movies) => {
+        return (
+          <div key = {movies.id}>
+          <img src={movies.poster} width={150} />
+
+       </div>
+        )  
+      })}
+    
         
-        <p>{JSON.stringify(allMovies)}</p>
+        
       </div>
      
     
-    </div>
+    
   )
 }
 
