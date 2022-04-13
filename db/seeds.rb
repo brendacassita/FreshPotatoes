bae6d4ac7818e20c255a869a55da482ed6fa7b26
Brenda <brendacassita@gmail.com>
2022-04-13 11:05:54 -0600

    merged

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,8 +17,8 @@ m1 = u1.movies.create(
   genre:'Comedy', 
   plot:'A process server and his marijuana dealer wind up on the run from hitmen and a corrupt police officer after he witnesses his dealers boss murder a competitor while trying to serve papers on him.',
   runtime:'1h 45m',
-  poster:'https://www.imdb.com/title/tt0910936/mediaviewer/rm3325203968/',
-  trailer:'https://www.imdb.com/video/vi2729574681?playlistId=tt0910936&ref_=tt_ov_vi')
+  poster:'https://m.media-amazon.com/images/I/5149l+O+P4L._AC_.jpg',
+  trailer:'https://www.youtube.com/watch?v=BWZt4v6b1hI')
 
 m2 = u1.movies.create(
   name:'The Interview',
@@ -26,7 +26,7 @@ m2 = u1.movies.create(
   genre:'Comedy', 
   plot:'Dave Skylark and his producer Aaron Rapaport run the celebrity tabloid show "Skylark Tonight". When they land an interview with a surprise fan, North Korean dictator Jong-Un Kim, they are recruited by the CIA to assassinate him.', 
   runtime:'1h 52m', 
-  poster:'https://www.imdb.com/title/tt2788710/mediaviewer/rm2264792576/', 
+  poster:'https://m.media-amazon.com/images/I/81R2FrgnfZL._AC_SL1000_.jpg', 
   trailer:'https://www.imdb.com/video/vi1114222361?playlistId=tt2788710&ref_=tt_ov_vi')
 
 m3 = u2.movies.create(
@@ -35,7 +35,7 @@ m3 = u2.movies.create(
   genre:'Action', 
   plot:'A historian races to find the legendary Templar Treasure before a team of mercenaries.', 
   runtime:'2h 25m', 
-  poster:'https://www.imdb.com/title/tt0368891/mediaviewer/rm751471616/', 
+  poster:'https://m.media-amazon.com/images/I/516L7A1oXZL._AC_.jpg', 
   trailer:'https://www.imdb.com/video/vi3890151705?playlistId=tt0368891&ref_=tt_ov_vi')
 
 m4 = u2.movies.create(
@@ -44,43 +44,43 @@ m4 = u2.movies.create(
   genre:'Mystery', 
   plot:'A detective investigates the death of the patriarch of an eccentric, combative family.', 
   runtime:'2h 10m', 
-  poster:'https://www.imdb.com/title/tt8946378/mediaviewer/rm2569376769/', 
+  poster:'https://m.media-amazon.com/images/I/71enm1zeBvL._AC_SL1500_.jpg', 
   trailer:'https://www.imdb.com/video/vi2464857881?playlistId=tt8946378&ref_=tt_ov_vi')
 
 m5 = u1.movies.create(
   name:'Jason and the Argonauts',
   year:'1963',
-  genre:'action', 
+  genre:'Action', 
   plot:'The legendary Greek hero leads a team of intrepid adventurers in a perilous quest for the legendary Golden Fleece.',
   runtime:'1h 44m', 
-  poster:'https://www.imdb.com/title/tt0057197/mediaviewer/rm2695511296/?ref_=tt_ov_i', 
+  poster:'https://m.media-amazon.com/images/I/71J-SyixPsL._AC_SL1000_.jpg', 
   trailer:'https://www.imdb.com/video/vi4187095321/?ref_=tt_vi_i_1')
 
 m6 = u1.movies.create(
   name:'Crazy Rich Asians',
   year:'2018', 
-  genre:'comedy',
+  genre:'Comedy',
   plot:'This contemporary romantic comedy, based on a global bestseller, follows native New Yorker Rachel Chu to Singapore to meet her boyfriends family.', 
   runtime:'2h 1m', 
-  poster:'https://www.imdb.com/title/tt3104988/mediaviewer/rm89476864/', 
+  poster:'https://m.media-amazon.com/images/I/91RSvcwSX+L._AC_SL1500_.jpg', 
   trailer:'https://www.youtube.com/watch?v=ZQ-YX-5bAs0')
 
 m7 = u1.movies.create(
   name:'Beetlejuice', 
   year:'1988', 
-  genre:'Comedy,Horror',
+  genre:'Comedy',
   plot:'The spirits of a deceased couple are harassed by an unbearable family that has moved into their home, and hire a malicious spirit to drive them out.', 
   runtime:'1h 32m', 
-  poster:'https://www.imdb.com/title/tt0094721/mediaviewer/rm2212181760/?ref_=tt_ov_i', 
+  poster:'https://m.media-amazon.com/images/I/7134I6+ZJmL._AC_SL1000_.jpg', 
   trailer:'https://www.imdb.com/video/vi4170302233?playlistId=tt0094721&ref_=tt_pr_ov_vi')
 
 m8 = u1.movies.create(
   name:'The Crow', 
   year:'1994', 
-  genre:'Action,Fantasy', 
+  genre:'Action', 
   plot:'A man brutally murdered comes back to life as an undead avenger of his and his fiancées murder.', 
   runtime:'1h 42m', 
-  poster:'https://www.imdb.com/title/tt0109506/mediaviewer/rm866189824/?ref_=tt_ov_i', 
+  poster:'https://m.media-amazon.com/images/I/71pdrpHZUfL._AC_SL1500_.jpg', 
   trailer:'https://www.imdb.com/video/vi404404249?playlistId=tt0109506&ref_=tt_ov_vi')
 
 m9 = u2.movies.create(
@@ -89,7 +89,7 @@ m9 = u2.movies.create(
   genre:'Adventure,Fantasy', 
   plot:'Sixteen-year-old Sarah is given thirteen hours to solve a labyrinth and rescue her baby brother Toby when her wish for him to be taken away is granted by the Goblin King Jareth.', 
   runtime:'1h 41m', 
-  poster:'https://www.imdb.com/title/tt0091369/mediaviewer/rm2649548544/?ref_=tt_ov_i', 
+  poster:'https://m.media-amazon.com/images/I/619yt12HbSL._AC_SL1001_.jpg', 
   trailer:'https://www.imdb.com/video/vi3636961561?playlistId=tt0091369&ref_=tt_ov_vi')
 
 m10 = u2.movies.create(
@@ -98,7 +98,7 @@ m10 = u2.movies.create(
   genre:'Drama,Romance', 
   plot:'Shakespeares famous play is updated to the hip modern suburb of Verona still retaining its original dialogue.', 
   runtime:'2h', 
-  poster:'https://www.imdb.com/title/tt0117509/mediaviewer/rm559635712/?ref_=tt_ov_i',  
+  poster:'https://m.media-amazon.com/images/I/81xn4q+XdeL._AC_SL1500_.jpg',  
   trailer:'https://www.imdb.com/video/vi58720537?playlistId=tt0117509&ref_=tt_pr_ov_vi')
 
 m11 = u1.movies.create(
@@ -125,7 +125,7 @@ m13 = u2.movies.create(
   genre:'Comedy', 
   plot:'A Southern California girl befriends three furry aliens after their spaceship lands in her swimming pool.', 
   runtime:'1h 40m', 
-  poster:'https://cdn.shopify.com/s/files/1/1416/8662/products/EarthGirlsareeasy_1989_original_film_art_1200x.jpg?v=1606251243', 
+  poster:'https://m.media-amazon.com/images/I/51Bw3Zfm97L.jpg', 
   trailer:'https://www.youtube.com/watch?v=3TnO5gz-1Wk')
 
 m14 = u2.movies.create(
@@ -140,37 +140,38 @@ m14 = u2.movies.create(
 m15 = u1.movies.create(
   name:'Man of Steel', 
   year:'2013', 
-  genre:'action', 
+  genre:'Action', 
   plot:'An alien child is evacuated from his dying world and sent to Earth to live among humans. His peace is threatened when other survivors of his home planet invade Earth.', 
   runtime:'2h 23m', 
-  poster:'https://www.imdb.com/title/tt0770828/mediaviewer/rm2035131904/?ref_=tt_ov_i', 
+  poster:'https://m.media-amazon.com/images/I/81wbOkjaZ+L._AC_SL1458_.jpg', 
   trailer:'https://www.imdb.com/video/vi705668633?playlistId=tt0770828&ref_=tt_ov_vi')
 
 m16 = u1.movies.create(
   name:'What Dreams May Come', 
   year:'1998', 
-  genre:'drama', 
+  genre:'Drama', 
   plot:'Chris Nielsen dies in an accident, and enters Heaven. But when he discovers that his beloved wife Annie has killed herself out of grief over the loss, he embarks on an afterlife adventure to reunite with her.', 
   runtime:'1h 53m', 
-  poster:'https://www.imdb.com/title/tt0120889/mediaviewer/rm11484160/?ref_=tt_ov_i', 
+  poster:'https://m.media-amazon.com/images/I/61-j8oLV9nL._AC_SL1500_.jpg', 
   trailer:'https://www.imdb.com/video/vi4110156057?playlistId=tt0120889&ref_=tt_pr_ov_vi')
 
 m17 = u1.movies.create(
   name:'Gladiator', 
   year:'2000', 
-  genre:'action', 
+  genre:'Action', 
   plot:'A former Roman General sets out to exact vengeance against the corrupt emperor who murdered his family and sent him into slavery.', 
   runtime:'2h 35m', 
-  poster:'https://www.imdb.com/title/tt0172495/mediaviewer/rm2442542592/?ref_=tt_ov_i', 
+  poster:'https://m.media-amazon.com/images/I/51a-Iz7wwCL._AC_.jpg', 
   trailer:'https://www.imdb.com/video/vi2628367897?playlistId=tt0172495&ref_=tt_pr_ov_vi')
 
 m18 = u1.movies.create(
   name:'The Count of Monte Cristo',
   year:'2002',
-  genre:'action',
+  genre:'Action',
   plot:'A young man, falsely imprisoned by his jealous "friend", escapes and uses a hidden treasure to exact his revenge.',
   runtime:'2h 11m',
-  poster:'https://www.imdb.com/title/tt0245844/mediaviewer/rm3545959936/?ref_=tt_ov_i')
+  poster:'https://m.media-amazon.com/images/I/41Byr2iM4mL._AC_.jpg',
+  trailer:'https://www.imdb.com/video/vi2628367897?playlistId=tt0172495&ref_=tt_pr_ov_vi')
 
 Review.create(
   rating:5,
