db240c19e3d4bf51ac42c4a7865962b798d03a28
Sharayah Hefner <rocknrayah@gmail.com>
2022-04-15 16:26:34 -0600

    dont delete

--- client/src/components/shared/NavBar.js
+++ client/src/components/shared/NavBar.js
@@ -171,7 +171,9 @@ const StyledInputBase = styled(InputBase)(({ theme }) => ({
                 }}
                 open={Boolean(anchorEl)}
                 onClose={handleMenuClose}
-              >
+            >
+              
+              {/* DO NOT DELETE THESE */}
                 <MenuItem onClick={handleMenuClose}><Link className='profilelink' to='/profile'>Profile</Link></MenuItem>
               <MenuItem onClick={handleMenuClose}><Link className='profilelink' to='/edit_profile'>Edit Profile</Link></MenuItem>
               <MenuItem onClick={handleMenuClose}>{renderRightNav()}</MenuItem>
