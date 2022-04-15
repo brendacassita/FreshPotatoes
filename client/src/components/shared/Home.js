517da56b6310e6aa709fb1c5f327a534807da5c8
Brenda <brendacassita@gmail.com>
2022-04-14 21:10:34 -0600

    home code update

--- client/src/components/shared/Home.js
+++ client/src/components/shared/Home.js
@@ -1,11 +1,13 @@
 import { AuthContext } from "../../providers/AuthProvider"
 import { useEffect, useState, useContext } from 'react'
 import axios from 'axios'
+import { useNavigate, Link } from "react-router-dom"
 
 
 
 const Home = ()=>{
     const [movies, setMovies] =  useState([])
+    const navigate = useNavigate()
 
     useEffect(()=>{
         getMovies()
@@ -20,19 +22,22 @@ const Home = ()=>{
         alert('error in getting movies')
         }
       }
+
+
+  const renderMovies = ()=>{
+    return movies.map((movie)=>(
+      <div key = {movie.id}>
+      <img src={movie.poster} width={150}/>
+      </div>
+    ))
+  }
   
+
+
       return(
         <div className='container'>
-          {movies.map((movie)=>{
-            return(
-              <div key = {movie.id}>
-                <img src={movie.poster} width={150} />
-                
-               
-    
-                </div>
-            )
-          })}
+         {renderMovies()}
+         
           {/* {JSON.stringify(movies)} */}
           
         </div>
