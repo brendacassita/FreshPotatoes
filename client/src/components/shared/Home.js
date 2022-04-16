e8a764d6d058cc38077285cc899f45b8cd62c2ee
Brenda <brendacassita@gmail.com>
2022-04-15 20:44:26 -0600

    merged

--- client/src/components/shared/Home.js
+++ client/src/components/shared/Home.js
@@ -88,12 +88,12 @@ const Home = ()=>{
                 </div>
                 <div>
                     <div style={{padding: 8}}>
-                        <img src="https://m.media-amazon.com/images/I/71pdrpHZUfL._AC_SL1500_.jpg" alt="placeholder" style={{width: '100%'}} />
+                        <img src="https://m.media-amazon.com/images/I/51a-Iz7wwCL._AC_.jpg" alt="placeholder" style={{width: '100%'}} />
                     </div>
                 </div>
                 <div>
                     <div style={{padding: 8}}>
-                        <img src="https://ariseletusbegoing.files.wordpress.com/2022/04/wp-16495315765356152001558212309618.jpg" alt="placeholder" style={{width: '100%'}} />
+                        <img src="https://m.media-amazon.com/images/I/81wbOkjaZ+L._AC_SL1458_.jpg" alt="placeholder" style={{width: '100%'}} />
                     </div>
                 </div>
                 <div>
