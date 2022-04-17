d862a8f3f8fc1f5341483f4e3fe65726ddf1f49d
Jessica Nguyen <jessica.stevens2115@gmail.com>
2022-04-16 19:39:26 -0600

    updated seeds file and added form pages

--- client/src/components/forms/ReviewForm.js
+++ client/src/components/forms/ReviewForm.js
@@ -3,9 +3,9 @@ import axios from 'axios'
 import { useParams, useLocation, useNavigate } from 'react-router-dom'
 
 const ReviewForm = (props) => {
-  const [rating, setRating] = useState(props.rating || '')
-  const [watched, setWatched] = useState(props.watched || '')
-  const [comment, setComment] = useState(props.comment || '')
+  // const [rating, setRating] = useState(props.rating || '')
+  // const [watched, setWatched] = useState(props.watched || '')
+  // const [comment, setComment] = useState(props.comment || '')
 }
 
 export default ReviewForm
