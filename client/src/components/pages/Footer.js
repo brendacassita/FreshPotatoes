438dd556a7a7fd85729af5d6b24f1b3e4f4b8e1a
Brenda <brendacassita@gmail.com>
2022-04-25 21:03:27 -0600

    back to the top button works

--- client/src/components/pages/Footer.js
+++ client/src/components/pages/Footer.js
@@ -4,6 +4,7 @@ import Container from '@mui/material/Container';
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 import { Link } from '@mui/material';
+import BackToTopButton from '../shared/scroll/BackToTopButton';
 
 
 const Footer = () => {
@@ -86,6 +87,7 @@ const Footer = () => {
   </Grid>
   <Box textAlign="center" pt={{xs: 5, sm: 10}} pb={{xs: 5, sm:0}}>
     Fresh Potatoes &reg; {new Date().getFullYear()}
+    <BackToTopButton />
   </Box>
   </Container>
 </Box>
