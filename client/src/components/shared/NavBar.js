880d42f6e0bd14609424ad3414db4338cd7295e0
Brenda <brendacassita@gmail.com>
2022-04-27 11:25:04 -0600

    removed small button on languages

--- client/src/components/shared/NavBar.js
+++ client/src/components/shared/NavBar.js
@@ -147,17 +147,7 @@ const Navbar = () => {
           <Link className="btn1" to="/SearchResults">
             Search
           </Link>
-          <button
-				className="navbar-toggler"
-				type="button"
-				data-toggle="collapse"
-				data-target="#navbarNav"
-				aria-controls="navbarNav"
-				aria-expanded="false"
-				aria-label="Toggle navigation"
-			>
-				<span className="navbar-toggler-icon"></span>
-			</button>
+     
 			<div className="collapse navbar-collapse" id="navbarNav">
 				<ul className="navbar-nav ml-auto">
 					<li className="nav-item">
