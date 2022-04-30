6f0c87e74f0ea8d9e9c4ec72624c40a7d1491676
Brenda <brendacassita@gmail.com>
2022-04-29 18:38:59 -0600

    merged

--- client/src/components/shared/NoMatch.js
+++ client/src/components/shared/NoMatch.js
@@ -1,13 +1,21 @@
 import { Link } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 
 
 
-const NoMatch = () => (
+const NoMatch = () => {
+  const navigate = useNavigate()
+  let nav = useNavigate()
+
+
+return (
   <div className='App1'>
   <h3>
     Page not found - return to
+    
     <Link to="/"><b style={{color: 'red'}}> HOME</b></Link>
   </h3></div>
 )
+}
 
 export default NoMatch;
