b00be6a85f77c30dafb4f0944fdd30cb22b43a3e
TrinaNix4 <98623374+TrinaNix4@users.noreply.github.com>
2022-04-29 21:43:43 -0600

    added loading state to edit profile button and added styling to register form

--- client/src/components/auth/EditProfile.js
+++ client/src/components/auth/EditProfile.js
@@ -1,7 +1,7 @@
 import React,{useContext,useEffect,useState} from "react";
 import '../CssFIles/editProfile.css'
 import {useTranslation} from 'react-i18next'
-
+import Button from "@mui/material/Button";
 
 // Import React FilePond
 import { FilePond, registerPlugin } from "react-filepond";
@@ -22,7 +22,7 @@ import bwPic from '../../Images/blackwhitePotatoe.png'
 
 // Register the plugins
 registerPlugin(FilePondPluginImageExifOrientation, FilePondPluginImagePreview);
-
+let timeout; 
 function EditProfile() {
   const [files, setFiles] = useState([]);
   const {  user, setUser } = useContext(AuthContext)
@@ -32,7 +32,7 @@ function EditProfile() {
   const [password, setPassword] = useState(user.password)
   const [username, setUserName] = useState(user.username)
   const {t} =  useTranslation(["profile", "common"])
-
+  const [loading, setLoading] = useState(false);
 
   const [showUpload, setShowUpload] = useState(false)
 
@@ -56,7 +56,6 @@ function EditProfile() {
       let data = new FormData()
       data.append('fileYO', files[0].file)
       data.append('name', name)
-      console.log(files)
      // axios call
      try{
         
@@ -69,92 +68,134 @@ function EditProfile() {
 
   const handleSubmit = async (e)=>{
     e.preventDefault()
+         setLoading(true);
+
+         timeout = setTimeout(() => {
+           setLoading(false);
+         }, 1000);
+       
    try{
-      console.log('trying to update with data:')
      let res = await axios.put(`/api/users/${user.id}`, {name, email, phone, password, username})
     setUser(res.data)
+    
    } catch(err){
        alert('error occured updating user info')
    } finally {
-     console.log('Password:',password)
-     console.log(files)
-     if(!files[0]){
-       
+     if(!files[0]){  
        return 
      }
      handleImage()
    }
 }
 
+useEffect(() => {
+  return () => {
+    if(timeout){
+        clearTimeout(timeout);
+    }
+  }
+},[])
+
 
   return (
     <div className="Appnow">
       <div className="borderfresh">
-        
-        <form className="editprofile form" onSubmit={handleSubmit} >
-         
-         <button className="profilechange" type='button' onClick={()=>setShowUpload(!showUpload)}> 
+        <form className="editprofile form" onSubmit={handleSubmit}>
+          <button
+            className="profilechange"
+            type="button"
+            onClick={() => setShowUpload(!showUpload)}
+          >
+            {user.avatar && (
+              <img className="avataredit" src={user.avatar} width={150} />
+            )}
+          </button>
 
-            {user.avatar && <img className="avataredit" src={user.avatar} width={150} />}</button>
-         
-          {!user.avatar && <button onClick={() => setShowUpload(!showUpload)} ><img src={bwPic} width='170px'></img></button>}
-          
+          {!user.avatar && (
+            <button onClick={() => setShowUpload(!showUpload)}>
+              <img src={bwPic} width="170px"></img>
+            </button>
+          )}
+
           {/* <p>image:</p> */}
           <div className="fileupload">
-        {showUpload && <FilePond
-            files={files}
-            allowMultiple={false}
-            onupdatefiles={handleUpdate}
-            labelIdle='Drag & Drop your files or <span class="filepond--label-action">Browse</span>'
-            />}
-            </div>
-       
-       <h2 className="usernameedit">{user.name}</h2> 
-        <p className="useremailedit"> {user.email}</p>
-        
-        {/* <p>{JSON.stringify(user)}</p> */}
-        
-        {/* <h5>Username: </h5><input value={username} onChange={(e)=> setUserName(e.target.value)} />  */}
-        {/* to do have *name already exists pop up if there is already a name */}
-        {/* <h6>*name already exists</h6>  */}
-           <div className="edit-all">
-        
-         
+            {showUpload && (
+              <FilePond
+                files={files}
+                allowMultiple={false}
+                onupdatefiles={handleUpdate}
+                labelIdle='Drag & Drop your files or <span class="filepond--label-action">Browse</span>'
+              />
+            )}
+          </div>
+
+          <h2 className="usernameedit">{user.name}</h2>
+          <p className="useremailedit"> {user.email}</p>
+
+          {/* <p>{JSON.stringify(user)}</p> */}
+
+          {/* <h5>Username: </h5><input value={username} onChange={(e)=> setUserName(e.target.value)} />  */}
+          {/* to do have *name already exists pop up if there is already a name */}
+          {/* <h6>*name already exists</h6>  */}
+          <div className="edit-all">
             <div className="editalign">
-            <h5 className="fullname">{t("profile:name")}</h5>
-              </div>
-              <input className="editbox" value={name} onChange={(e) => setName(e.target.value)} /> 
-            
-            <div  className="editalign">
+              <h5 className="fullname">{t("profile:name")}</h5>
+            </div>
+            <input
+              className="editbox"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+
+            <div className="editalign">
               <h5 className="fullname">{t("profile:email")}</h5>
-              </div>
-            <input className="editbox" value={email} onChange={(e) => setEmail(e.target.value)} /> 
-            
-            
+            </div>
+            <input
+              className="editbox"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+
             <div className="editalign">
               <h5 className="fullname">{t("profile:password")}</h5>
-              </div>
-            <input className="editbox" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />  
+            </div>
+            <input
+              className="editbox"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
             <div className="editalign">
               <h5 className="fullname">{t("profile:phone")}</h5>
-              </div>
-            <input className="editbox" value={phone} onChange={(e) => setPhone(e.target.value)} />  
-       
-          
-        <br/>   
-        <br/>   
+            </div>
+            <input
+              className="editbox"
+              value={phone}
+              onChange={(e) => setPhone(e.target.value)}
+            />
 
-       
-        
-        <button className="editprofilebtn" type='submit'>{t("common:submit")}</button>
-        
-        <br />
-        </div>
-        {/* <button  type = 'button' onClick={()=>setShowUpload(!showUpload)}>Click to update profile image</button> */}
-        
+            <br />
+            <br />
+            <Button
+              className="buttonlogin1 btnlogin"
+              variant="contained"
+              type="button"
+              onClick={handleSubmit}
+              disabled={loading}
+            >
+              {loading ? "Loading..." : "Update Profile"}
+            </Button>
+
+            {/* BRENDA: here is the section I couldn't get the loading state from above to work */}
+            {/* <button className="editprofilebtn" type="submit">
+              {t("common:submit")}
+            </button> */}
+            <br />
+          </div>
+          {/* <button  type = 'button' onClick={()=>setShowUpload(!showUpload)}>Click to update profile image</button> */}
         </form>
-        </div>
-     </div>
+      </div>
+    </div>
   );
 }
 
